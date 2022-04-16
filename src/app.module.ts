0df44814357a39f9b12a0c91dda7494a86d59425
guilherme correa <guilhermescorrea@hotmail.com.br>
2022-04-16 13:05:33 -0300

    refactor: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './database/database.module';
 import { HttpModule } from './http/http.module';
 
 @Module({
-  imports: [DatabaseModule, HttpModule],
+  imports: [HttpModule],
   controllers: [],
   providers: [],
 })
