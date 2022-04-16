ea9343208e1c61dba5b58ee2ca46fe5eeec25fef
guilherme correa <guilhermescorrea@hotmail.com.br>
2022-04-16 13:02:34 -0300

    feat: authorization guard and user req param

---
+++ src/http/auth/current-user.param.ts
@@ -0,0 +1,16 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export interface AuthUser {
+  sub: string;
+}
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, context: ExecutionContext): AuthUser => {
+    const { req } = GqlExecutionContext.create(context).getContext();
+
+    console.log(req.user);
+
+    return req.user;
+  },
+);
