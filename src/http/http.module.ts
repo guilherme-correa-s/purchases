a934de8ffac37693ca2e22047f4557a647181e4c
guilherme correa <guilhermescorrea@hotmail.com.br>
2022-04-16 13:05:20 -0300

    feat: add http module

---
+++ src/http/http.module.ts
@@ -0,0 +1,32 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'node:path';
+import { DatabaseModule } from 'src/database/database.module';
+import { CustomersService } from 'src/services/customers.service';
+import { ProductsService } from 'src/services/products.service';
+import { PurchasesService } from 'src/services/purchases.service';
+import { CustomersResolver } from './graphql/resolvers/customers.resolver';
+import { ProductsResolver } from './graphql/resolvers/products.resolver';
+import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  providers: [
+    ProductsResolver,
+    ProductsService,
+    PurchasesResolver,
+    PurchasesService,
+    CustomersResolver,
+    CustomersService,
+  ],
+})
+export class HttpModule {}
